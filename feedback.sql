de9427ef6ebde394b7433b5265ed7cad2e57b279
Lily Gitbugger <xianj.chen@qq.com>
2021-03-06 06:35:09 +0800

    2nd

--- feedback.sql
+++ feedback.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS feedback CASCADE;
 DROP TYPE IF EXISTS status;
-CREATE TYPE status AS ENUM ('created', 'waiting', 'terminated');
+CREATE TYPE STATUS AS ENUM ('created', 'waiting', 'terminated');
 CREATE TABLE feedback(
    feedback_id SERIAL,
    customer_id INT,
    order_id INT,
    product_id INT,
-   create_on timestamp,
-   last_process timestamp,
+   create_on TIMESTAMP,
+   last_process TIMESTAMP,
    discription VARCHAR(255) NOT NULL,
-   process_status status,
+   process_status STATUS,
    PRIMARY KEY(feedback_id)
 );

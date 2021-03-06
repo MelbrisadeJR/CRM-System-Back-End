7d6ca5cd144c9926d2b70e640de5943ce052edd1
Lily Gitbugger <xianj.chen@qq.com>
2021-03-06 09:06:04 +0800

    CRM_project_initialized
    initializing

---
+++ CRM_project/src/test/java/com/melbrisade/project/CrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melbrisade.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

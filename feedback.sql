ee51000287980286f5691953547630aeeffeccfa
Lily Gitbugger <xianj.chen@qq.com>
2021-03-06 10:46:48 +0800

    review_change

--- feedback.sql
+++ feedback.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS feedback CASCADE;
-DROP TYPE IF EXISTS status;
-CREATE TYPE STATUS AS ENUM ('created', 'waiting', 'terminated');
+DROP TYPE IF EXISTS FEEDBACK_STATUS;
+CREATE TYPE FEEDBACK_STATUS AS ENUM ('created', 'waiting', 'terminated');
 CREATE TABLE feedback(
    feedback_id SERIAL,
    customer_id INT,
@@ -9,6 +9,6 @@ CREATE TABLE feedback(
    create_on TIMESTAMP,
    last_process TIMESTAMP,
    discription VARCHAR(255) NOT NULL,
-   process_status STATUS,
+   process_status FEEDBACK_STATUS,
    PRIMARY KEY(feedback_id)
 );
--- order.sql
+++ order.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS order CASCADE;
-CREATE TABLE order
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders
 (
     adjustment_price numeric NOT NULL,
     discount numeric NOT NULL,
-    order_id integer NOT NULL 
+    order_id integer NOT NULL,
     promotion numeric NOT NULL,
     purchase_date date NOT NULL,
     purchase_number bigint NOT NULL,
     shipment_price numeric NOT NULL,
     total_price numeric NOT NULL,
     "GST" numeric NOT NULL,
-    CONSTRAINT PRIMARY KEY (order_id)
+    PRIMARY KEY (order_id)
 )

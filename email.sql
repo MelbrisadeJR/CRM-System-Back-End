bbb4564a9fcfd4314c194a2978e31cbae9f2c374
DizzyRaisin <daneezhao@gmail.com>
2021-03-06 22:37:46 +1100

    remove quotes in table field

--- email.sql
+++ email.sql
@@ -1,24 +1,26 @@
-DROP TABLE IF EXISTS "email" CASCADE;
-CREATE TABLE IF NOT EXISTS "email" (
-    "id" BIGINT,
-    "subject" VARCHAR(255),
-    "template" SMALLINT,
-    "status" SMALLINT,
-    "attachment_url" VARCHAR(255),
-    "content" TEXT,
-    "recipient" VARCHAR(255),
-    "sent_time" TIMESTAMP NOT NULL,
-    PRIMARY KEY ("id")
+DROP TABLE IF EXISTS email CASCADE;
+CREATE TABLE IF NOT EXISTS email (
+    id BIGINT,
+    subject VARCHAR(255),
+    template SMALLINT,
+    status SMALLINT,
+    attachment_url VARCHAR(255),
+    content TEXT,
+    recipient VARCHAR(255),
+    sent_time TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
     );
-DROP TABLE IF EXISTS "status" CASCADE;
-CREATE TABLE IF NOT EXISTS "status" (
-    "id" SMALLINT,
-    "type" VARCHAR(255),
-    PRIMARY KEY ("id")
+
+DROP TABLE IF EXISTS status CASCADE;
+CREATE TABLE IF NOT EXISTS status (
+    id SMALLINT,
+    type VARCHAR(255),
+    PRIMARY KEY (id)
     );
-DROP TABLE IF EXISTS "template" CASCADE;
-CREATE TABLE IF NOT EXISTS "template" (
-    "id" SMALLINT,
-    "type" VARCHAR(255),
-    PRIMARY KEY ("id")
+
+DROP TABLE IF EXISTS template CASCADE;
+CREATE TABLE IF NOT EXISTS template (
+    id SMALLINT,
+    type VARCHAR(255),
+    PRIMARY KEY (id)
     );

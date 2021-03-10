723c706d5326525727a27bf76882c6c24e7735c7
Danni Zhao <daneezhao@gmail.com>
2021-03-10 22:32:08 +1100

    get email list, post email

---
+++ CRM_project/src/main/java/com/melbrisade/project/controllers/EmailController.java
@@ -0,0 +1,33 @@
+package com.melbrisade.project.controllers;
+
+import com.melbrisade.project.dtos.EmailGetDto;
+import com.melbrisade.project.dtos.EmailPostDto;
+import com.melbrisade.project.entities.Email;
+import com.melbrisade.project.services.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/emails")
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<EmailGetDto> createCustomer(@RequestBody EmailPostDto emailPostDto) {
+        EmailGetDto emailGetDto = emailService.createEmail(emailPostDto);
+        return ResponseEntity.ok(emailGetDto);
+    }
+
+    @GetMapping //
+    public List<Email> getCustomerList() {
+        return emailService.getEmailList();
+    }
+
+//    @PutMapping
+
+//    @DeleteMapping
+}

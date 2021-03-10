ae60e17b99864faa472308a6c64748f0e9b0f846
Lily Bugger <xianj.chen@qq.com>
2021-03-10 09:27:12 +0800

    Database Script Arranged

---
+++ DatabaseScript/order.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE order
+(
+    adjustment_price numeric NOT NULL,
+    discount numeric NOT NULL,
+    order_id integer NOT NULL,
+    promotion numeric NOT NULL,
+    purchase_date date NOT NULL,
+    purchase_number bigint NOT NULL,
+    shipment_price numeric NOT NULL,
+    total_price numeric NOT NULL,
+    "GST" numeric NOT NULL,
+    PRIMARY KEY (order_id)
+    FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
+)

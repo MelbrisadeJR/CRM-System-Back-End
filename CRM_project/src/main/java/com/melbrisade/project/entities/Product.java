60a8ef94ac25aecbc5dc6fdf5f56774a39cf5378
Tutuzz <73582134+Tutuzz@users.noreply.github.com>
2021-03-17 06:45:06 +1100

    Update Product.java

--- CRM_project/src/main/java/com/melbrisade/project/entities/Product.java
+++ CRM_project/src/main/java/com/melbrisade/project/entities/Product.java
@@ -43,3 +43,98 @@ public class Product{
     private byte image;
 
 }
+
+public Product() {
+}
+
+public Product(String product_name, int stock,  float discounts, int SKU, float product_price, float product_cost, String category, String manufacture, byte image){
+    this.product_name = product_name;
+    this.stock = stock;
+    this.discounts = discounts;
+    this.SKU = SKU;
+    this.product_price = product_price;
+    this.product_cost = product_cost;
+    this.category = category;
+    this.manufacture = manufacture;
+    this.image = image;
+}
+
+public Long getProduct_id(){
+    return product_id;
+}
+
+public void setProduct_id(Long product_id){
+    this.product_id = product_id;
+}
+
+public int getStock(){
+    return stock;
+}
+
+public void setStock(int stock){
+    this.stock = stock;
+}
+
+public float getDiscounts(){
+    return discounts;
+}
+
+public void setDiscounts(float discounts){
+    this.discounts = discounts;
+}
+
+public int getSKU(){
+    return SKU;
+}
+
+public void setSKU(int SKU){
+    this.SKU = SKU;
+}
+
+public float getProduct_price(){
+    return product_price;
+}
+
+public void setProduct_price(float product_price){
+    this.product_price = product_price;
+}
+
+public float getProduct_cost(){
+    return product_cost;
+}
+
+public void setProduct_cost(float product_cost){
+    this.product_cost = product_cost;
+}
+
+public String getCategory(){
+    return category;
+}
+
+public void setCategory(String category){
+    this.category = category;
+}
+
+public String getManufacture(){
+    return manufacture;
+}
+
+public void setManufacture(String manufacture){
+    this.manufacture = manufacture;
+}
+
+public String getProduct_name(){
+    return product_name;
+}
+
+public void setProduct_name(String product_name){
+    this.product_name = product_name;
+}
+
+public byte getImage(){
+    return image;
+}
+
+public void setImage(byte image){
+    this.image = image;
+}

50587301327a7c3d9d873dce06f7d6674a50a483
Tutuzz <73582134+Tutuzz@users.noreply.github.com>
2021-03-17 07:35:41 +1100

    Update ProductRepository.java

--- CRM_project/src/main/java/com/melbrisade/project/repositories/ProductRepository.java
+++ CRM_project/src/main/java/com/melbrisade/project/repositories/ProductRepository.java
@@ -4,4 +4,5 @@ import com.melbrisade.project.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  Product findProductByProduct_name(String product_name);
 }

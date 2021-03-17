ff5f8770130ec5576d6dc57d36bf86442f3953f5
Harry Qin <49329053+Kvroneko@users.noreply.github.com>
2021-03-17 11:53:41 +1100

    dtos

---
+++ CRM_project/src/main/java/com/melbrisade/project/dtos/OrderGetDto.java
@@ -0,0 +1,10 @@
+package com.melbrisade.project.dtos;
+
+import lombok.Data;
+
+@Data
+public class OrderGetDto {
+    private String promotion;
+    private String discount;
+}
+
---
+++ CRM_project/src/main/java/com/melbrisade/project/dtos/OrderPostDto.java
@@ -0,0 +1,9 @@
+package com.melbrisade.project.dtos;
+
+import lombok.Data;
+
+@Data
+public class OrderPostDto {
+    private String promotion;
+    private String discount;
+}
